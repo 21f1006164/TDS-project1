664fdcdda1f905e42ac7af6e9e5d4314a45650cc
esakkirajar <21f1006164@ds.study.iitm.ac.in>
2025-02-13 19:59:07 +0000

    coded in app

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
-# This is project app file
+# /// script 
+# requires-python = ">=3.12"
+# dependencies = [
+#          "fastapi",
+#          "uvicorn",
+#          "requests",
+# ]
+# ///
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware (
+    CORSMiddleware,
+    allow_orgin = ['*'],
+    allow_credentials = True,
+    allow_methods = ['GET', 'POST'],
+    allow_headers = ['*']
+)
+
+@app.get("/")
+def home():
+    return{"Text to be displayed"}
+
+
+if ___name__ == '__main__':
+    import uvicorn
+    uvicorn.run (app, host="0.0.0.0", port=8000)
